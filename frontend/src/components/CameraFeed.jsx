e9ac4d218cc159480a93e000db04ad468090d148
jshrihankumar-dev <jshrihankumar-dev@users.noreply.github.com>
2026-05-17 15:53:24 -0400

    imports

--- frontend/src/components/CameraFeed.jsx
+++ frontend/src/components/CameraFeed.jsx
@@ -1,5 +1,6 @@
-<div>
-  <video />
-  <canvas />
-  <button>Start Camera</button>
-</div>
+import { useEffect, useRef, useState } from "react";
+import { Pose, POSE_CONNECTIONS } from "@mediapipe/pose";
+import { Camera } from "@mediapipe/camera_utils";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { analyzePosture } from "../lib/postureAnalysis";
+
